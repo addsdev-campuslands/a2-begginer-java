73d58d3bbf35b6d9ad4f475010bd3573521ee37f
Adrian Ruiz <addsdeveloper@gmail.com>
2025-11-11 13:10:04 -0500

    feat: :sparkles: add Camper, Factorial, and Trainer classes; enhance Ahorcado game with new features

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -2,6 +2,9 @@ package com.adrian;
 
 import java.util.Scanner;
 
+import com.adrian.ejercicios.Camper;
+import com.adrian.ejercicios.Factorial;
+import com.adrian.ejercicios.Trainer;
 import com.adrian.game.ahoracado.Ahorcado;
 
 public class Main {
@@ -11,19 +14,73 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
         var ahoracado = new Ahorcado();
+        var factorial = new Factorial();
         while (flag) {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
                             ** Ingrese la opcion deseada **
                     1. Jugar al Ahoracado
+                    2. Palabras Adivinadas
                     0. Salir
                     """);
             switch (scan.nextInt()) {
                 case 1:
                     
-                    ahoracado.iniciar();
+                    ahoracado.iniciar("X");
                     ahoracado.ejecutar(scan);
                     break;
+                case 2:
+                    System.out.println(ahoracado.palabrasAdivinadas);
+                    break;
+                case 3:
+                    //Administrativo
+                    //Trainer
+                    //Colabarador
+                    Camper camper = new Camper("Andrues","Perrea",16,
+                    "TI", "10051234512",
+                    "?","adrian@gmail.com","12345600");
+                    System.out.println(camper.telefono);
+                    /*camper.nombre = "Andrues";
+                    camper.apellido = "Perrea";
+                    camper.edad = 16;
+                    camper.tipoDocumento = "TI";
+                    camper.documento = "1005123456";
+                    camper.estratoSocial = -1;
+                    camper.genero = "?";
+                    camper.email = "andresitto_777@gmail.com";*/
+                    camper.telefono = "018000918080";
+                    System.out.println(camper.telefono);
+                    //camper.nivelIngles = 10;
+
+                    camper.dormir();
+                    camper.divertirse();
+
+                    camper.cambiarNombre("Poncio Zuleta");
+                    camper.reportar();
+                    camper.calificar();
+
+
+                    break;
+
+                case 4:
+                    Trainer trainer = new Trainer();
+                    trainer.nombre = "Adriaaaaann";
+                    trainer.apellido = "Nunez";
+                    /*trainer.edad = 27;
+                    trainer.tipoDocumento = "CC";
+                    trainer.documento = "1097101921";
+                    trainer.estratoSocial = 8;
+                    trainer.genero = "masculino";
+                    trainer.email = "adriannnnn00@gmail.com";
+                    trainer.telefono = "018000918080";
+                    trainer.skill = "Software";*/
+
+
+                    trainer.dormir();
+                    trainer.divertirse();
+                    trainer.eseniar();
+                    trainer.tomacafe();
+
                 case 0:
                     // Salir
                     flag = false;
--- src/main/java/com/adrian/game/ahoracado/Ahorcado.java
+++ src/main/java/com/adrian/game/ahoracado/Ahorcado.java
@@ -1,14 +1,17 @@
 package com.adrian.game.ahoracado;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ahorcado {
-    String palabraSecreta, palabraTablero; // Palabra secreta a adivinar
-    String[] palabras; // Array de palabras posibles
-    int intentos; // Número máximo de intentos permitidos
-    int errores; // Número de errores cometidos
+    private String palabraSecreta, palabraTablero; // Palabra secreta a adivinar
+    private String[] palabras; // Array de palabras posibles
+    public String[] palabrasAdivinadas; // Array de palabras adivinadas
+    public String[] palabrasUsadas; // Array de palabras usadas
+    private int intentos; // Número máximo de intentos permitidos
+    private int errores; // Número de errores cometidos
 
     public Ahorcado() { // Metodo especial - Constructor
         palabras = new String[] { "programacion",
@@ -18,7 +21,12 @@ public class Ahorcado {
         // AQUI
         palabraTablero = "";
         errores = 0;
-        intentos = 0;
+        intentos = 5;
+        palabrasUsadas = new String[28];
+    }
+
+    public int verErrorres() {
+        return errores;
     }
 
     public void iniciar() {
@@ -32,12 +40,27 @@ public class Ahorcado {
         // ejecutar();
     }
 
+    public void iniciar(String caracter) {
+        var r = new Random();
+        palabraSecreta = palabras[r.nextInt(palabras.length - 1)];// interfaz
+        var flag = caracter + " ";
+        palabraTablero = "";
+        for (int a = 0; a < palabraSecreta.length(); a++) {
+            palabraTablero += flag; // _ _ _ _ _ _ _ _
+        }
+        // ejecutar();
+    }
+
     public void ejecutar(Scanner scan) {
 
         while (true) {
+            System.out.println("\f");
+            System.out.println(mensaje(palabrasUsadas));
             System.out.println(mensaje());
+
             var letra = scan.next();
             var temporal = "";
+            guardarLetra(letra);
             if (palabraSecreta.contains(letra)) {
                 // a
                 // _ _ _ _ _ _ _ _
@@ -46,22 +69,82 @@ public class Ahorcado {
                     if (String.valueOf(palabraSecreta.charAt(i)).equals(letra)) {
                         // ________
                         // 0123456
-                        //Cambio _ > a
+                        // Cambio _ > a
                         letras[i] = letra.charAt(0); // _a__a_a_
-                        
+
                     }
                 }
-                //asignar
+                // asignar
                 for (char c : letras) {
                     temporal += String.valueOf(c) + " ";
                 }
                 palabraTablero = temporal;
 
+                if (validarJuego()) {
+                    System.out.println("GANO");
+                    break;
+                }
+
             } else {
+
+                errores++;
+                if (validarGameOver()) {
+                    System.out.println("GAME OVER");
+                    System.out.println("La palabra era: " + palabraSecreta);
+                    break;
+                }
+
                 System.out.println("Ahhh Erroooor");
                 System.out.println("Ta'maaaaaaaal");
             }
         }
+    }
+
+    private void guardarLetra(String letra) {
+        if (existeLetra(letra)) return;
+
+        for (int a = 0; a < palabrasUsadas.length; a++) {
+
+            var usada = palabrasUsadas[a];
+
+            if (usada == null) {
+                palabrasUsadas[a] = letra;
+                break;
+            }
+
+        }
+    }
+
+    private boolean existeLetra(String letraBuscada) {
+        for (String letra : palabrasUsadas) {
+            if (letra != null && letra.equals(letraBuscada)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean validarGameOver() {
+        return intentos < errores;
+    }
+
+    private boolean validarJuego() {
+        return palabraTablero.replace(" ", "").equals(palabraSecreta);
+    }
+
+    public String mensaje(String[] data) {
+        StringBuilder strBuild = new StringBuilder();
+        strBuild.append("**************\n");
+        strBuild.append("\n");
+        for (String item : data) {
+            if (item == null)
+                break;
+            strBuild.append(item);
+            strBuild.append(",");
+        }
+        strBuild.append("\n");
+        strBuild.append("**************\n");
+        return strBuild.toString();
     }
 
     public String mensaje() {
@@ -73,8 +156,6 @@ public class Ahorcado {
         strBuild.append(errores);
         strBuild.append("\n");
         strBuild.append(palabraTablero);
-        strBuild.append("\n");
-        strBuild.append(palabraSecreta);
         return strBuild.toString();
     }
 
