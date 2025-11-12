1b12e08498087353eb8b31506adac60352dcb9fd
Adrian Ruiz <addsdeveloper@gmail.com>
2025-11-12 13:10:55 -0500

    feat: :sparkles: update user password check

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -2,38 +2,35 @@ package com.adrian;
 
 import java.util.Scanner;
 
-import com.adrian.ejercicios.bank.BankAccount;
 
 public class Main {
     final static int maxRange = 10000;
 
     public static void main(String[] args) {
-        // Crea una clase BankAccount con el atributo privado balance.
-        // Implementa los métodos deposit(double amount) y withdraw(double amount)
-        // que validen las cantidades correctamente.
+        /**
+         * Crea una clase User con los atributos privados username y password. 
+         * Implementa los métodos setUsername(String username), 
+         * setPassword(String password) y checkPassword(String inputPassword) que compare contraseñas.
+         */
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
-        var account = new BankAccount();
+        
         while (flag) {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
                             ** Ingrese la opcion deseada **
-                    1. Consignar
-                    2. Retirar
-                    3. Ver Saldo
+                    1. 
                     0. Salir
                     """);
             switch (scan.nextInt()) {
                 case 1:
-                    System.out.println("Ingrese el monto en numeros enteros a depositar: ");
-                    account.deposit(scan.nextInt());
+                    
                     break;
                 case 2:
-                    System.out.println("Ingrese el monto en numeros enteros a retirar: ");
-                    account.withdraw(scan.nextInt());
+                    
                     break;
                 case 3:
-                    System.out.println("Su Saldo es: $"+account.showBalance());
+                    
                     break;
                 case 0:
                     // Salir
