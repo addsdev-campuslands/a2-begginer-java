27e15af1c35b472c44a9ff532644418564f4ef49
Adrian Ruiz <addsdeveloper@gmail.com>
2025-11-12 12:41:22 -0500

    feat: :sparkles: implement BankAccount class for banking operations

--- src/main/java/com/adrian/Main.java
+++ src/main/java/com/adrian/Main.java
@@ -2,93 +2,47 @@ package com.adrian;
 
 import java.util.Scanner;
 
-import com.adrian.ejercicios.Camper;
-import com.adrian.ejercicios.Factorial;
-import com.adrian.ejercicios.Trainer;
-import com.adrian.game.ahoracado.Ahorcado;
+import com.adrian.ejercicios.bank.BankAccount;
 
 public class Main {
     final static int maxRange = 10000;
 
     public static void main(String[] args) {
+        // Crea una clase BankAccount con el atributo privado balance.
+        // Implementa los métodos deposit(double amount) y withdraw(double amount)
+        // que validen las cantidades correctamente.
         Scanner scan = new Scanner(System.in);
         boolean flag = true;
-        var ahoracado = new Ahorcado();
-        var factorial = new Factorial();
+        var account = new BankAccount();
         while (flag) {
             System.out.println("""
                     ++++++++++++++  MENU  ++++++++++++++
                             ** Ingrese la opcion deseada **
-                    1. Jugar al Ahoracado
-                    2. Palabras Adivinadas
+                    1. Consignar
+                    2. Retirar
+                    3. Ver Saldo
                     0. Salir
                     """);
             switch (scan.nextInt()) {
                 case 1:
-                    
-                    ahoracado.iniciar("X");
-                    ahoracado.ejecutar(scan);
+                    System.out.println("Ingrese el monto en numeros enteros a depositar: ");
+                    account.deposit(scan.nextInt());
                     break;
                 case 2:
-                    System.out.println(ahoracado.palabrasAdivinadas);
+                    System.out.println("Ingrese el monto en numeros enteros a retirar: ");
+                    account.withdraw(scan.nextInt());
                     break;
                 case 3:
-                    //Administrativo
-                    //Trainer
-                    //Colabarador
-                    Camper camper = new Camper("Andrues","Perrea",16,
-                    "TI", "10051234512",
-                    "?","adrian@gmail.com","12345600");
-                    System.out.println(camper.telefono);
-                    /*camper.nombre = "Andrues";
-                    camper.apellido = "Perrea";
-                    camper.edad = 16;
-                    camper.tipoDocumento = "TI";
-                    camper.documento = "1005123456";
-                    camper.estratoSocial = -1;
-                    camper.genero = "?";
-                    camper.email = "andresitto_777@gmail.com";*/
-                    camper.telefono = "018000918080";
-                    System.out.println(camper.telefono);
-                    //camper.nivelIngles = 10;
-
-                    camper.dormir();
-                    camper.divertirse();
-
-                    camper.cambiarNombre("Poncio Zuleta");
-                    camper.reportar();
-                    camper.calificar();
-
-
+                    System.out.println("Su Saldo es: $"+account.showBalance());
                     break;
-
-                case 4:
-                    Trainer trainer = new Trainer();
-                    trainer.nombre = "Adriaaaaann";
-                    trainer.apellido = "Nunez";
-                    /*trainer.edad = 27;
-                    trainer.tipoDocumento = "CC";
-                    trainer.documento = "1097101921";
-                    trainer.estratoSocial = 8;
-                    trainer.genero = "masculino";
-                    trainer.email = "adriannnnn00@gmail.com";
-                    trainer.telefono = "018000918080";
-                    trainer.skill = "Software";*/
-
-
-                    trainer.dormir();
-                    trainer.divertirse();
-                    trainer.eseniar();
-                    trainer.tomacafe();
-
                 case 0:
                     // Salir
                     flag = false;
 
                     break;
                 default:
-                    System.out.println("Ingrese una opcion "+
-                    "valida......\nPresione Enter para continuar.");
+                    System.out.println("Ingrese una opcion " +
+                            "valida......\nPresione Enter para continuar.");
                     scan.nextLine();
                     break;
             }
--- src/main/java/com/adrian/game/ahoracado/Ahorcado.java
+++ src/main/java/com/adrian/game/ahoracado/Ahorcado.java
@@ -1,7 +1,5 @@
 package com.adrian.game.ahoracado;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
